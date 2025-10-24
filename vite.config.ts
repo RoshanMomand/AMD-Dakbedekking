7aa9904468e5d21224074d067ecaa6069ff9fd4b
roshanmomand <1028086@hr.nl>
2025-10-24 19:28:08 +0200

    adding the sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import sitemap from "vite-plugin-sitemap";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: "https://amddakbedekking.nl/sitemap.xml",
+    }),
+  ],
+
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
 });
